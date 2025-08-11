73b651e4944adba93d4d49499070000f52860a6d
rzmsq <semin.02@mail.ru>
2025-08-11 16:33:24 +0300

    l3_2 update

--- l2_2/main.go
+++ l2_2/main.go
@@ -8,20 +8,29 @@ import (
 )
 
 func main() {
+	// Создание массива с числами
 	arr := [...]int{2, 4, 6, 8, 10}
+	// Создание WaitGroup для ожидания завершения всех горутин
 	var wg sync.WaitGroup
 
+	// Запуск горутин для каждого элемента массива
 	for _, i := range arr {
+		// Увеличение счетчика WaitGroup
 		wg.Add(1)
+		// Запуск горутины для обработки числа
 		go func(val int) {
+			// Уменьшение счетчика WaitGroup при завершении горутины
 			defer wg.Done()
+			// Печать квадрата числа в стандартный вывод
 			_, err := os.Stdout.Write([]byte(strconv.Itoa(val*val) + "\n"))
+			// Проверка на ошибки при записи в стандартный вывод
 			if err != nil {
 				log.Fatalf("error writing to stdout: %v", err)
 				return
 			}
-		}(i)
+		}(i) // передача значения в горутину
 	}
 
+	// Ожидание завершения всех горутин
 	wg.Wait()
 }
