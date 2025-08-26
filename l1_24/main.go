5c0f6b65976fd11968e9581466689e9c12eb1b77
rzmsq <semin.02@mail.ru>
2025-08-26 11:23:08 +0300

    l1_24

--- l1_24/main.go
+++ l1_24/main.go
@@ -10,11 +10,7 @@ type Point struct {
 	x, y float64
 }
 
-func (p *Point) NewPoint(x, y float64) {
-	p.x, p.y = x, y
-}
-
-func (p *Point) Distance(dest Point) float64 {
+func (p *Point) Distance(dest *Point) float64 {
 	return math.Sqrt(math.Pow(dest.x-p.x, 2) + math.Pow(dest.y-p.y, 2))
 }
 
@@ -22,12 +18,14 @@ func (p *Point) PrintPoints() {
 	fmt.Println("Точка имеет координаты: ", p.x, p.y)
 }
 
+func NewPoint(x, y float64) *Point {
+	return &Point{x, y}
+}
+
 func main() {
 
-	var point1, point2 Point
-
-	point1.NewPoint(rand.Float64()+float64(rand.Intn(100)), rand.Float64()+float64(rand.Intn(100)))
-	point2.NewPoint(rand.Float64()+float64(rand.Intn(100)), rand.Float64()+float64(rand.Intn(100)))
+	point1 := NewPoint(rand.Float64()+float64(rand.Intn(100)), rand.Float64()+float64(rand.Intn(100)))
+	point2 := NewPoint(rand.Float64()+float64(rand.Intn(100)), rand.Float64()+float64(rand.Intn(100)))
 
 	fmt.Print("1: ")
 	point1.PrintPoints()
